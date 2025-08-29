b8d07fbd5fbdc22d2fb388ae9232abada5744340
Alvaro Sanchez <alvarosanchezpalomino@gmail.com>
2025-08-30 00:19:13 +0200

    refactor

--- Makefile
+++
@@ -1,9 +0,0 @@
-CC = cc
-STD = c11
-CFLAGS = -std=$(STD) -Wall -Wextra -pedantic
-build b:
-	$(CC) $(CFLAGS) main.c -o bee
-run r: build
-	@./bee /home/alvaro/ws/bee/main.c
-clean c:
-	rm bee
---
+++ build.sh
@@ -0,0 +1,13 @@
+CC=cc
+STD=c99
+CFLAGS="-std=$STD -Wall -Wextra -pedantic"
+
+if [ "$#" -eq 0 ] || [ "$1" = 'small' ]; then
+	echo small
+	$CC $CFLAGS bee.c -o bee
+elif [ "$1" = 'tiny' ]; then
+	echo tiny
+	$CC $CFLAGS bee_tiny.c -o bee
+elif [ "$1" = 'clean' ]; then
+	rm bee
+fi
