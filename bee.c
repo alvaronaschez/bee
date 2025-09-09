8df7060c5d77ae56eb4949f6dbbfd18acc282314
Alvaro Sanchez <alvarosanchezpalomino@gmail.com>
2025-09-09 22:12:23 +0200

    stuff

--- bee.c
+++ bee.c
@@ -21,14 +21,15 @@
 #include <wchar.h>
 #include <locale.h>
 
-#define DEBUG
+//#define DEBUG
 #define LOCALE "en_US.UTF-8"
 #define fg_color TB_YELLOW
 #define bg_color TB_BLACK
 #define tablen 8
 #define footerheight 1
-const char *footer_format = "<%s> \"%s\"  [=%d] L%d C%d";
-const char *debug_footer_format = "<%s> \"%s\"  [=%d] L%d C%d bx:%d leftcol:%d vxgoal:%d";
+//const char *footer_format = "<%s> [%s] l%d/%d C%d";
+const char *footer_format = "<%s> [%s] (%d/%d, %d)";
+const char *debug_footer_format = "<%s> [%s]  L%d/%d C%d bx:%d leftcol:%d vxgoal:%d";
 #define screen_height (tb_height() - footerheight)
 #define screen_width (tb_width())
 
@@ -214,11 +215,11 @@ static inline void print_screen(const struct bee *bee){
   // print footer
 #ifdef DEBUG 
   tb_printf(0, tb_height() - 1, bg_color, fg_color, debug_footer_format,
-            mode_label[bee->mode], bee->filename, bee->buf_len, bee->y, bee->vx,
+            mode_label[bee->mode], bee->filename, bee->y, bee->buf_len, bee->vx,
             bee->bx, bee->leftcol, bee->vxgoal);
 #else
   tb_printf(0, tb_height() - 1, bg_color, fg_color, footer_format,
-            mode_label[bee->mode], bee->filename, bee->buf_len, bee->y, bee->vx);
+            mode_label[bee->mode], bee->filename, bee->y, bee->buf_len, bee->vx);
 #endif
   // print cursor
   tb_set_cursor(bee->vx - bee->leftcol, bee->y - bee->toprow);
