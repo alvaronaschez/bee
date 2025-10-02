471c2536076fbcecd93c4667f655f0778dc9edae
Alvaro Sanchez <alvarosanchezpalomino@gmail.com>
2025-10-02 23:21:12 +0200

    refactor

--- script/test.sh
+++ script/test.sh
@@ -1,4 +1,3 @@
-cd $PWD
-cc -c src/text.c -o obj/text.o
-cc -I $PWD/src test/test_text.c obj/*.o -o out/test_text
+cc -c text.c -o obj/text.o
+cc test_text.c obj/*.o -o out/test_text
 ./out/test_text
